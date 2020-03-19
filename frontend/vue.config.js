734f812e97dc6eb1119dc08578b2892daa5c5af6
ALCUK <62395758+ALCUK@users.noreply.github.com>
2020-03-20 02:03:25 +0900

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     transpileDependencies: ['vuetify'],
     publicPath: '/ALCUK/',
-    outputDir: '../backend/public/',
     devServer: {
         proxy: {
             '/api': {
